bfd3b87bfc5a37dd6083496d816cc4f7962ac0c5
valyo-marinov <valyo.grozev@gmail.com>
2025-04-17 22:00:00 +0300

    running code

--- fetch_data.php
+++ fetch_data.php
@@ -1,48 +1,28 @@
-<<<<<<< HEAD
 <?php
 $api_key = 'da5ccaf3255e57986ac9ff4d';
-$base_currency = 'USD';
 
-$target_currency = 'BGN';
+$from = $_GET['from'] ?? 'USD';
+$to = $_GET['to'] ?? 'BGN';
+$amount = $_GET['amount'] ?? 1;
 
-$url = "https://v6.exchangerate-api.com/v6/$api_key/latest/$base_currency";
+
+$url = "https://v6.exchangerate-api.com/v6/$api_key/latest/$from";
+
+
 $response = file_get_contents($url);
 
 if ($response !== false) {
     $data = json_decode($response, true);
 
-
-    if (isset($data['conversion_rates'][$target_currency])) {
-        $rate = $data['conversion_rates'][$target_currency];
-        echo "1 $base_currency = $rate $target_currency";
+   
+    if (isset($data['conversion_rates'][$to])) {
+        $rate = $data['conversion_rates'][$to];
+        $converted = round($amount * $rate, 2);
+        echo "<strong>Резултат:</strong><br>$amount $from = $converted $to";
     } else {
-        echo "Валутата $target_currency не е намерена.";
+        echo "Валутата $to не е намерена.";
     }
 } else {
     echo "Грешка при извличане на данни от API.";
 }
-=======
-<?php
-$api_key = 'da5ccaf3255e57986ac9ff4d';
-$base_currency = 'USD';
-
-$target_currency = 'BGN';
-
-$url = "https://v6.exchangerate-api.com/v6/$api_key/latest/$base_currency";
-$response = file_get_contents($url);
-
-if ($response !== false) {
-    $data = json_decode($response, true);
-
-
-    if (isset($data['conversion_rates'][$target_currency])) {
-        $rate = $data['conversion_rates'][$target_currency];
-        echo "1 $base_currency = $rate $target_currency";
-    } else {
-        echo "Валутата $target_currency не е намерена.";
-    }
-} else {
-    echo "Грешка при извличане на данни от API.";
-}
->>>>>>> ca78743599d20703ede018748886e71fa520d9c8
 ?>
